9aacc7bd1025e09cefa178342a983b662c1d787e
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-13 18:08:31 +0530

    remove people

--- AddrBook.cs
+++ AddrBook.cs
@@ -64,75 +64,75 @@ namespace AddressBooks
         //    Console.WriteLine("EmailId: " + person.EmailId);
         //    Console.WriteLine("-------------------------------------------");
         //}
-        public static void Modify()
-        {
-            if (People.Count != 0)
-            {
-                Console.WriteLine("Enter the contact to modify:");
-                string Modified = Console.ReadLine();
-                foreach (var person in People)
-                {
-                    if (person.FirstName.ToUpper() == Modified.ToUpper())
-                    {
-                        while (true)
-                        {
-                            Console.WriteLine("Enter the option to modify the property: ");
-                            Console.WriteLine("Enter 1 to Change First name ");
-                            Console.WriteLine("Enter 2 to Change Last name ");
-                            Console.WriteLine("Enter 3 to Change Phone Number ");
-                            Console.WriteLine("Enter 4 to Change Address ");
-                            Console.WriteLine("Enter 5 to Change City ");
-                            Console.WriteLine("Enter 6 to Change State ");
-                            Console.WriteLine("Enter 7 to Change Pincode ");
-                            Console.WriteLine("Enter 8 to Exit ");
-                            int Check = Convert.ToInt32(Console.ReadLine());
-                            switch (Check)
-                            {
-                                case 1:
-                                    Console.WriteLine("Enter the New First Name: ");
-                                    person.FirstName = Console.ReadLine();
-                                    break;
-                                case 2:
-                                    Console.WriteLine("Enter the New Last Name: ");
-                                    person.LastName = Console.ReadLine();
-                                    break;
-                                case 3:
-                                    Console.WriteLine("Enter the New Phone Number: ");
-                                    person.PhoneNum = Console.ReadLine();
-                                    break;
-                                case 4:
-                                    Console.WriteLine("Enter the New Address: ");
-                                    person.Addresses = Console.ReadLine();
-                                    break;
-                                case 5:
-                                    Console.WriteLine("Enter the New City: ");
-                                    person.City = Console.ReadLine();
-                                    break;
-                                case 6:
-                                    Console.WriteLine("Enter the New State: ");
-                                    person.State = Console.ReadLine();
-                                    break;
-                                case 7:
-                                    Console.WriteLine("Enter the New Pin Code: ");
-                                    person.ZipCode = Console.ReadLine();
-                                    break;
-                                case 8:
-                                    return;
+        //public static void Modify()
+        //{
+        //    if (People.Count != 0)
+        //    {
+                //Console.WriteLine("Enter the contact to modify:");
+                //string Modified = Console.ReadLine();
+                //foreach (var person in People)
+                //{
+                //    if (person.FirstName.ToUpper() == Modified.ToUpper())
+                //    {
+                //        while (true)
+                //        {
+                //            Console.WriteLine("Enter the option to modify the property: ");
+                //            Console.WriteLine("Enter 1 to Change First name ");
+                //            Console.WriteLine("Enter 2 to Change Last name ");
+                //            Console.WriteLine("Enter 3 to Change Phone Number ");
+                //            Console.WriteLine("Enter 4 to Change Address ");
+                //            Console.WriteLine("Enter 5 to Change City ");
+                //            Console.WriteLine("Enter 6 to Change State ");
+                //            Console.WriteLine("Enter 7 to Change Pincode ");
+                //            Console.WriteLine("Enter 8 to Exit ");
+                            //int Check = Convert.ToInt32(Console.ReadLine());
+                            //switch (Check)
+                            //{
+                            //    case 1:
+                            //        Console.WriteLine("Enter the New First Name: ");
+                            //        person.FirstName = Console.ReadLine();
+                            //        break;
+                            //    case 2:
+                            //        Console.WriteLine("Enter the New Last Name: ");
+                            //        person.LastName = Console.ReadLine();
+                            //        break;
+                            //    case 3:
+                            //        Console.WriteLine("Enter the New Phone Number: ");
+                            //        person.PhoneNum = Console.ReadLine();
+                            //        break;
+                            //    case 4:
+                            //        Console.WriteLine("Enter the New Address: ");
+                            //        person.Addresses = Console.ReadLine();
+                            //        break;
+                            //    case 5:
+                            //        Console.WriteLine("Enter the New City: ");
+                            //        person.City = Console.ReadLine();
+        //                            break;
+        //                        case 6:
+        //                            Console.WriteLine("Enter the New State: ");
+        //                            person.State = Console.ReadLine();
+        //                            break;
+        //                        case 7:
+        //                            Console.WriteLine("Enter the New Pin Code: ");
+        //                            person.ZipCode = Console.ReadLine();
+        //                            break;
+        //                        case 8:
+        //                            return;
 
-                            }
+        //                    }
 
-                        }
+        //                }
 
-                    }
-                    else
-                    {
-                        Console.WriteLine("Enter the valid name!");
-                    }
-                }
+        //            }
+        //            else
+        //            {
+        //                Console.WriteLine("Enter the valid name!");
+        //            }
+        //        }
 
 
-            }
-        }
+        //    }
+        //}
         //public static void ListingPeople()
         //{
         //    if (People.Count == 0)
@@ -149,5 +149,24 @@ namespace AddressBooks
         //    Console.WriteLine("\nPress any key to continue.");
         //    Console.ReadKey();
         //}
+
+        //Removing the detail
+        public static void RemovePeople()
+        {
+            Console.WriteLine("Enter the first name of the person you would like to remove.");
+            string Remove = Console.ReadLine();
+            foreach (var person in People.ToList())
+            {
+                if (person.FirstName.ToUpper() == Remove.ToUpper())
+                {
+                    People.Remove(person);
+                    Console.WriteLine("Contact is deleted");
+                }
+                else
+                {
+                    Console.WriteLine("Contact is not present");
+                }
+            }
+        }
     }
 }
